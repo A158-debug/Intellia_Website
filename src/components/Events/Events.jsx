252870c5beb0825b8ff5f6304021a13200e263ca
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2023-03-01 23:35:40 +0530

    make responsive

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,103 +1,54 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./Events.css";
-// import {
-//   motion,
-//   useScroll,
-//   useSpring,
-//   useTransform,
-//   useMotionValue,
-//   useVelocity,
-//   useAnimationFrame,
-// } from "framer-motion";
-// import { wrap } from "@motionone/utils";
-// import events from "../../img/events.jfif";
+import events from "../../img/bg7.jpg";
+import { motion } from "framer-motion";
+import { FiExternalLink } from "react-icons/fi";
 
-// function ParallaxText({ children, baseVelocity = 100 }) {
-//   const baseX = useMotionValue(0);
-//   const { scrollY } = useScroll();
-//   const scrollVelocity = useVelocity(scrollY);
-//   const smoothVelocity = useSpring(scrollVelocity, {
-//     damping: 50,
-//     stiffness: 400,
-//   });
-//   const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-//     clamp: false,
-//   });
-
-//   /**
-//    * This is a magic wrapping for the length of the text - you
-//    * have to replace for wrapping that works for you or dynamically
-//    * calculate
-//    */
-//   const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
-
-//   const directionFactor = useRef(1);
-//   useAnimationFrame((t, delta) => {
-//     let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
-
-//     /**
-//      * This is what changes the direction of the scroll once we
-//      * switch scrolling directions.
-//      */
-//     if (velocityFactor.get() < 0) {
-//       directionFactor.current = -1;
-//     } else if (velocityFactor.get() > 0) {
-//       directionFactor.current = 1;
-//     }
-
-//     moveBy += directionFactor.current * moveBy * velocityFactor.get();
-
-//     baseX.set(baseX.get() + moveBy);
-//   });
-
-//   /**
-//    * The number of times to repeat the child text should be dynamically calculated
-//    * based on the size of the text and viewport. Likewise, the x motion value is
-//    * currently wrapped between -20 and -45% - this 25% is derived from the fact
-//    * we have four children (100% / 4). This would also want deriving from the
-//    * dynamically generated number of children.
-//    */
-//   return (
-//     <div className="parallax">
-//       <motion.div className="scroller" style={{ x }}>
-//         <span>{children} </span>
-//         <span>{children} </span>
-//         <span>{children} </span>
-//         <span>{children} </span>
-//       </motion.div>
-//     </div>
-//   );
-// }
-
-// const department_logos = [
-//   { id: 1, photo: events, name: "Tech" },
-//   { id: 2, photo: events, name: "Cult" },
-//   { id: 3, photo: events, name: "Sports" },
-// ];
+const department_logos = [
+  { id: 1, photo: events, name: "Tech" },
+  { id: 2, photo: events, name: "Cult" },
+  { id: 3, photo: events, name: "Sports" },
+];
 
 const Events = () => {
   return (
-    <div>
+    <div className="py-10">
       <p className="text-7xl font-semibold text-white text-center">
         OUR EVENTS
       </p>
-        {/* <div className="scrolling_text  max-w-full text-white">
-          <ParallaxText baseVelocity={-5}>Framer Motion</ParallaxText>
-        <ParallaxText baseVelocity={5}>Scroll velocity</ParallaxText>
-         
-        </div> */}
-         {/* <div class="flex flex-row flex-wrap mt-10 justify-center content-center p-3 md:p-0">
-            {department_logos.map((e) => (
-              <div className="bg-white text-teal-700 m-2 ">
-                <div className="">
-                  <img src={e.photo} alt="" srcset="" className="object-cover w-96 h-56"/>
-                </div>
-                <div className="p-2">
-                  <p className="text-2xl text-center">{e.name} </p>
-                </div>
-              </div>
-            ))}
-          </div> */}
+
+      <div class="flex flex-row flex-wrap mt-10 justify-center content-center p-3 md:p-0">
+        {department_logos.map((e) => (
+          <div className="bg-white text-teal-700 m-2 ">
+            <div className="app__work-img">
+              <img src={e.photo} alt="" className="object-cover w-96 h-56" />
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{
+                  duration: 0.25,
+                  ease: "easeInOut",
+                  staggerChildren: 0.5,
+                }}
+                className="app__work-hover"
+              >
+                <a href="./" target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.9] }}
+                    transition={{ duration: 0.25 }}
+                    className="events_icon"
+                  >
+                    <FiExternalLink />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
+            <div className="p-2">
+              <p className="text-2xl text-center">{e.name} </p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,179 +2,41 @@ import React from "react";
 import bg2 from "../../img/bg2.png";
 import "./Header.css";
 import video1 from "../../img/videos/video1.mp4";
-import { BsEmojiSmile } from "react-icons/bs";
-import CountDown from "../CountDown/CountDown";
 
 const Header = () => {
   return (
     <>
       <div className="relative">
+        {/*------- Video ------- */}
         <video loop muted autoPlay className="back__video">
-        <source src={video1} type="video/mp4" />
-      </video>
-      <div className="flex flex-row h-screen ">
-        <div className=" basis-full flex justify-center content-center">
-          <div className="content text-white self-center text-center">
-            <p className="text-3xl department_heading">
-              BOARD OF DEPARTMENTAL SOCIETIES
-            </p>
-            <p className="text-xl">IIT JODHPUR</p>
-            <p>presents</p>
-            <p
-              className="text-9xl my-2 intellia_heading"
-              style={{
-                color: "#fff",
-                textShadow: "0 0 7px ",
-              }}
-            >
-              Intellia
-            </p>
-            <p className="text-xl pl-8">
-              INTER DEPARTMENTAL GENERAL CHAMPIONSHIP{" "}
-            </p>
-          </div>
-        </div>
-        <div className="basis-0 md:basis-5/12 flex p-10">
-          <img src={bg2} alt="" className="self-end" />
-        </div>
-      </div>
-        <div
-          className="flex justify-center content-center flex-wrap text-white p-5 gap-4"
-          style={{
-            background:
-              "radial-gradient(circle, rgba(62,192,182,1) 0%, rgba(15,91,86,1) 100%)",
-          }}
-        >
-          <div className=" flex ">
-            <div className="text-5xl p-2 pt-5">
-              <BsEmojiSmile />
-            </div>
-            <div>
-              <h1 className="text-7xl font-semibold">232</h1>
-              <p className="text-xl department_heading my-1">Happy Clients</p>
-              <p className="footer_license_text">Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div className=" flex ">
-            <div className="text-5xl p-2 pt-5">
-              <BsEmojiSmile />
-            </div>
-            <div>
-              <h1 className="text-7xl font-semibold">232</h1>
-              <p className="text-xl department_heading my-1">Happy Clients</p>
-              <p className="footer_license_text">Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div className="flex ">
-            <div className="text-5xl p-2 pt-5">
-              <BsEmojiSmile />
-            </div>
-            <div>
-              <h1 className="text-7xl font-semibold">232</h1>
-              <p className="text-xl department_heading my-1">Happy Clients</p>
-              <p className="footer_license_text">Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-          <div className="flex ">
-            <div className="text-5xl p-2 pt-5">
-              <BsEmojiSmile />
-            </div>
-            <div>
-              <h1 className="text-7xl font-semibold">232</h1>
-              <p className="text-xl department_heading my-1">Happy Clients</p>
-              <p className="footer_license_text">Lorem ipsum dolor sit amet.</p>
-            </div>
-          </div>
-        </div>
-        {/* <div className="mt-10 text-9xl text-white font-semibold" style={{fontFamily: 'Raleway'}}>
-         <div className="flex"><p>HOW</p></div>
-         <div className="flex relative">
-          <p className="absolute left-20 ">THIS</p>  
-         </div>
-         <div className="flex p-10 relative">
-          <p className="absolute left-40 top-28">WORKS</p>  
-         </div>
-      </div> */}
-      </div>
-      <div className="flex flex-col md:flex-row text-white relative mt-20 p-3 md:p-10">
-        <div className="basis-12 md:basis-6/12 flex flex-col pr-10 home_wallpaper">
-          <div className="">
-            <p className="text-9xl font-semibold p-2 mr-6 float-right">T</p>
-          </div>
-          <div className="">
-            <p className="text-9xl font-semibold p-2 mr-7 float-right">E</p>
-          </div>
-          <div className="">
-            <p className="text-9xl font-semibold p-2 mr-4 float-right">A</p>
-          </div>
-          <div className="">
-            <p className="text-9xl font-semibold p-2 float-right">M</p>
-          </div>
-        </div>
-        <div className="basis-12 md:basis-6/12 p-2 md:p-5 text-white text-center">
-          <p className="intellia_heading_about">Intellia</p>
-          <p className="footer_license_text">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-            incidunt sit veritatis doloremque rerum porro in. Dolores blanditiis
-            natus quasi eligendi reiciendis voluptatum assumenda, accusantium
-            incidunt temporibus, recusandae dolore delectus?
-          </p>
+          <source src={video1} type="video/mp4" />
+        </video>
 
-          <div className="flex justify-center content-center mt-10 ">
-            <button
-              className="border-none bg-slate-400 rounded-sm p-3"
-              style={{ background: "#3ec0b6", col: "white" }}
-            >
-              Send Message
-            </button>
-          </div>
-          <div className="mt-10">
-            <CountDown deadline="March, 17, 2023" />
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col md:flex-row text-white">
-        <div className="basis-6/12 p-5 flex justify-center content-center">
-          <p
-            className="text-4xl md:text-7xl font-bold flex self-center pl-10"
-            style={{ lineHeight: "80px" }}
-          >
-            Lorem ipsum dolor sit amet, consec adipisicing elit{" "}
-          </p>
-        </div>
-        <div className="basis-6/12 flex flex-col content-around justify-center">
-          <div className="flex justify-center content-center my-10 ml-16">
-            <div className="text-5xl p-2">
-              <BsEmojiSmile />
-            </div>
-            <div className="flex self-center">
-              <p className="footer_license_text">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Suscipit deserunt voluptatum sapiente magni eius. Accusantium.{" "}
+        {/*------- Home Page Heading --------*/}
+        <div className="flex flex-row h-screen ">
+          <div className="basis-7/12 flex justify-center content-center">
+            <div className="content text-white self-center text-center">
+              <p className="text-4xl department_heading">
+                BOARD OF DEPARTMENTAL SOCIETIES
+              </p>
+              <p className="text-xl">IIT JODHPUR</p>
+              <p className="">presents</p>
+              <p
+                className="intellia_heading text-9xl leading md:text-[230px] md:leading-[250px]"
+                style={{
+                  color: "#fff",
+                  textShadow: "0 0 7px ",
+                }}
+              >
+                Intellia
+              </p>
+              <p className="text-2xl pl-8 font-semibold">
+                INTER DEPARTMENTAL GENERAL CHAMPIONSHIP
               </p>
             </div>
           </div>
-          <div className="flex justify-center content-center my-10 ml-24 ">
-            <div className="text-5xl p-2">
-              <BsEmojiSmile />
-            </div>
-            <div className="flex self-center">
-              <p className="footer_license_text">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Suscipit deserunt voluptatum sapiente magni eius. Accusantium.{" "}
-              </p>
-            </div>
-          </div>
-          <div className="flex justify-center content-center my-10 ml-16">
-            <div className="text-5xl p-2">
-              <BsEmojiSmile />
-            </div>
-            <div className="flex self-center">
-              <p className="footer_license_text">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-                Suscipit deserunt voluptatum sapiente magni eius. Accusantium.{" "}
-              </p>
-            </div>
+          <div className="basis-5/12 md:flex p-10 hidden ">
+            <img src={bg2} alt="" className="self-end" />
           </div>
         </div>
       </div>
@@ -183,3 +45,16 @@ const Header = () => {
 };
 
 export default Header;
+
+// eslint-disable-next-line no-lone-blocks
+{
+  /* <div className="mt-10 text-9xl text-white font-semibold" style={{fontFamily: 'Raleway'}}>
+<div className="flex"><p>HOW</p></div>
+<div className="flex relative">
+ <p className="absolute left-20 ">THIS</p>  
+</div>
+<div className="flex p-10 relative">
+ <p className="absolute left-40 top-28">WORKS</p>  
+</div>
+</div> */
+}
