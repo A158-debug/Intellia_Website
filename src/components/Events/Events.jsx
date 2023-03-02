a74e96e7cc000892672cf3d4711470f98a2edec3
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2023-03-02 19:27:56 +0530

    add events

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./Events.css";
 import events from "../../img/bg7.jpg";
-import { motion } from "framer-motion";
 import { FiExternalLink } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 const department_logos = [
   { id: 1, photo: events, name: "Tech" },
@@ -19,26 +19,26 @@ const Events = () => {
 
       <div class="flex flex-row flex-wrap mt-10 justify-center content-center p-3 md:p-0">
         {department_logos.map((e) => (
-          <div className="bg-white text-teal-700 m-2 ">
-            <div className="app__work-img">
+          <div className="bg-white text-teal-700 m-2 app__work-item ">
+            <div className="relative ">
               <img src={e.photo} alt="" className="object-cover w-96 h-56" />
               <motion.div
+                className="app__work-hover flex justify-center items-center"
                 whileHover={{ opacity: [0, 1] }}
                 transition={{
                   duration: 0.25,
                   ease: "easeInOut",
                   staggerChildren: 0.5,
                 }}
-                className="app__work-hover"
               >
-                <a href="./" target="_blank" rel="noreferrer">
+                <a href="./" target="_blank" rel="noreferrer" className="">
                   <motion.div
+                    className=" text-3xl flex  text-white"
                     whileInView={{ scale: [0, 1] }}
                     whileHover={{ scale: [1, 0.9] }}
                     transition={{ duration: 0.25 }}
-                    className="events_icon"
                   >
-                    <FiExternalLink />
+                    <FiExternalLink  />
                   </motion.div>
                 </a>
               </motion.div>
