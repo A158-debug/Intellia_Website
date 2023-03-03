01e58af5231a098f99ced11543110faa2d9d5ae4
Abhishek Kumar <76657113+A158-debug@users.noreply.github.com>
2023-03-03 23:19:19 +0530

    style cards

--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,70 +1,76 @@
 import React, { useState } from "react";
-import bg7 from '../../img/bg7.jpg';
+import bg7 from "../../img/bg7.jpg";
 
 const total_events = [
   [
-    { 
-      id:1,
+    {
+      id: 1,
       name: "Tech Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7,
-      register_link:"./"
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
+      register_link: "./",
     },
-    { 
-      id:2,
+    {
+      id: 2,
       name: "Tech Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-    { 
-      id:3,
+    {
+      id: 3,
       name: "Tech Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-   
   ],
   [
-    { 
-      id:1,
+    {
+      id: 1,
       name: "Sports Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-    { 
-      id:2,
+    {
+      id: 2,
       name: "Sports Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-    { 
-      id:3,
+    {
+      id: 3,
       name: "SportsEvent Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-   
   ],
   [
-    { 
-      id:1,
+    {
+      id: 1,
       name: "Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-    { 
-      id:2,
+    {
+      id: 2,
       name: "Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-    { 
-      id:3,
+    {
+      id: 3,
       name: "Event Name",
-      description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
-      img:bg7
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit. orem ipsum, dolor sit amet consectetur adipisicing elit.",
+      img: bg7,
     },
-   
   ],
 ];
 
@@ -79,11 +85,11 @@ const Events = () => {
 
   return (
     <>
-      <div className="pt-10">
+      <div className="pt-20">
         <p className="text-4xl md:text-7xl font-semibold text-white text-center">
           OUR EVENTS
         </p>
-        <div className="flex justify-center mb-4 mt-10 md:mt-20">
+        <div className="flex justify-center mb-4 mt-10 md:mt-10">
           {tabs.map((tab) => (
             <button
               key={tab.id}
@@ -96,21 +102,29 @@ const Events = () => {
             </button>
           ))}
         </div>
-        <div className="p-4 flex flex-col items-center ">
-            {total_events[activeTab].map((item) => (
-              <div className="flex text-white p-5 mt-10 w-4/5 border" key={item.id}>
-                  <div className="basis-1/2">
-                    <p className="intellia_heading_about text-6xl">{item.name}</p>
-                    <p className="py-3 px-2 footer_license_text text-lg">{item.description}</p>
-                    <p className="border text-xl footer_license_text w-7/12 text-center py-3 rounded cursor-pointer mx-auto">Register</p>
-                  </div>
-                  <div className="basis-1/2">
-                    <img src={item.img} alt="" className="object-cover h-48 w-96" />
-                  </div>
+        <div className="p-4 flex flex-col items-center pb-10">
+          {total_events[activeTab].map((item) => (
+            <div
+              className="flex text-white p-5 mt-10 w-4/5 events_animated_border"
+              key={item.id}
+              style={{background: 'rgba(255, 255, 255, 0.09)'}}
+            >
+              <div className="basis-1/2">
+                <p className="intellia_heading_about text-6xl">{item.name}</p>
+                <p className="py-3 px-2 footer_license_text text-lg">
+                  {item.description}
+                </p>
 
+                <div class="">
+                  <a href="./" className="register_button mx-auto"><span>Register</span></a>
+                </div>
               </div>
-            ))}
-      </div>
+              <div className="basis-1/2">
+                <img src={item.img} alt="" className="object-cover h-48 w-96" />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
